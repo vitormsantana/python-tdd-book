4e986b7c4dbdd38bc5536a41d909bfb6788ac931
Vitor Santana <vitormsantana25@gmail.com>
2024-07-23 00:52:45 -0300

    feature: adding new to-do list improvements

--- lists/tests.py
+++ lists/tests.py
@@ -10,16 +10,11 @@ class SmokeTest(TestCase):
 """
 
 class HomePageTest(TestCase):
-    """
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-        self.assertTemplateUsed(response, 'home.html')
-    """
-    
     def test_uses_home_template(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
