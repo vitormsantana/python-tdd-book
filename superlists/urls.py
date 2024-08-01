7a5dd665b5649969729d9d6f5ae64b63e89a9d93
Vitor Santana <vitormsantana25@gmail.com>
2024-08-01 19:54:28 -0300

    adding other url file and modifying the original one

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from lists.views import home_page
+from django.conf.urls import url, include
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    path("", home_page, name="home"),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls)),
 ]
